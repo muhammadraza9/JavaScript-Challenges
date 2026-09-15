2b4516fdedaef5f7ac0dafaf3ce963d4f0395483
Muhammad Raza <imuhammadraza53@gmail.com>
2026-09-15 23:11:42 +0500

    Add table containing  multiplication tables challenge

---
+++ challenge2.js
@@ -0,0 +1,10 @@
+// Print a table containing multiplication tables
+
+
+let table = 10;
+
+for(let i = 1; i <= table; i++){
+        for(let n = 1; n <= 10; n++){
+              console.log(`${i} X ${n} = ${i * n}`);
+        };      
+};
