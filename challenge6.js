2d64fe09e7962375b630d21cc1ce8b4504c1dff7
Muhammad Raza <imuhammadraza53@gmail.com>
2026-09-16 16:53:48 +0500

    again Add sort an array

--- challenge6.js
+++ challenge6.js
@@ -1,7 +1,7 @@
 //  Sort an array from lowest to highest
 
 
-const array = [8,6,5,9,3,4,1,12,22,44];
+const array = [8,6,-5,9,-3,4.5,-1,12.5,12.2,22.7,22.5,44];
 
 array.sort();
 //array.sort((a,b) => a - b);
