5c3ac09cca21ce85cd16d7ac68e5588aa88c23e4
Muhammad Raza <imuhammadraza53@gmail.com>
2026-09-16 15:25:32 +0500

    Add length converter function

---
+++ challenge3.js
@@ -0,0 +1,18 @@
+// Create a length converter function
+
+
+function converter(kilometers){
+              return kilometers * 0.62137 ;
+}
+
+console.log(converter(3) + " miles");
+
+///
+
+let kilometers = 5;
+
+function convrt(){
+          return kilometers * 0.62137 ;    
+};
+
+console.log(convrt());
