e658be287b7dd7decb8481603720c0401ab38931
Muhammad Raza <imuhammadraza53@gmail.com>
2026-09-16 17:43:57 +0500

    Add return boolean if a number is divisible

---
+++ challenge9.js
@@ -0,0 +1,16 @@
+// Return a Boolean if a number is divisible by 10
+
+
+const number = 22;
+
+console.log(number % 10 === 0);
+
+////
+
+const num = 30 ;
+
+function divd(num){
+          return num % 10 === 0 ;    
+};
+
+console.log(divd(num));
